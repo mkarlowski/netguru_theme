b5ce12569278136b0288e9d5def5efc4446a82e9
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-11-11 20:48:27 +0100

    allow to pass style as param

--- lib/netguru_theme/generator.rb
+++ lib/netguru_theme/generator.rb
@@ -4,28 +4,38 @@
 
 module NetguruTheme
   class Generator < Thor
+    option :style
+
     desc 'install', 'Install netguru_theme styles into your project'
     def install
-      render = Sass::Engine.new(File.read(path), params).render
+      return unless valid_style?
+      render = Sass::Engine.new(File.read(source_path), params).render
       File.write('netguru-theme.min.css', render)
     end
 
     private
 
-    def path
-      File.join(top_level_directory, 'app', 'assets', 'stylesheets', '_netguru-theme.scss')
+    def valid_style?
+      return true if [:compressed, :nested, :expanded, :compact].include? style
+      puts('Invalid --style option')
     end
 
-    def top_level_directory
-      File.dirname(File.dirname(File.dirname(__FILE__)))
+    def source_path
+      top_level_directory = File.dirname(File.dirname(File.dirname(__FILE__)))
+      File.join(top_level_directory, 'app', 'assets', 'stylesheets',
+                '_netguru-theme.scss')
+    end
+
+    def style
+      options[:style].nil? ? :compressed : options[:style]
     end
 
     def params
       {
         cache: false,
         syntax: :scss,
-        style: :compressed,
-        filename: path
+        style: style,
+        filename: source_path
       }
     end
   end
